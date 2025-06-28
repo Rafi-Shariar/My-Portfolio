a4deecba9dc89e455f62e9ebda0c0aaded4219a5
Rafi-Shariar <rafi.shariar619@gmail.com>
2025-06-28 21:44:07 +0600

    project closed

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -3,7 +3,10 @@ import { IoPersonCircleOutline } from "react-icons/io5";
 
 const AboutMe = () => {
   return (
-    <section id="about" className="max-w-5xl mx-auto h-[300px]  bg-secondary/20 rounded-3xl mt-10">
+    <section
+      id="about"
+      className="max-w-5xl mx-auto h-[400px]  bg-secondary/20 rounded-3xl mt-10"
+    >
       <div className="max-w-4xl mx-auto text-center p-5">
         {/* Icon and Title */}
         <div className="flex items-center justify-center gap-2 mb-6">
@@ -22,14 +25,21 @@ const AboutMe = () => {
           </p>
           <br />
           <p>
-            My journey began during my school days in Khulna, where I developed
-            a strong interest in programming. Over time, I discovered a love for
-            building user-friendly applications that solve real-world problems.
-          </p>
+            My programming journey started back in school, where I discovered a
+            strong curiosity for solving problems with code. I enjoy crafting
+            user-friendly applications that address real-world challenges,
+            whether that’s building small tools or complex systems.
+          </p> <br />
           <p>
-            I’m driven by the challenge of creating impactful solutions — from
-            small tools to full systems — using technology to improve everyday
-            life.
+            I thrive when working on projects that allow me to combine
+            creativity and logic, especially those that require collaboration
+            and thoughtful time management to meet deadlines.
+          </p> <br />
+          <p>
+            Outside of programming, I love traveling to new places and immersing
+            myself in different cultures. Music is another passion of mine—it
+            helps me relax and stay inspired. I value a balanced lifestyle that
+            nurtures both my technical skills and personal growth.
           </p>
           <br />
         </div>
